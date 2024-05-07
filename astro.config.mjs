b18834798b97f64174e3c80719d8f6b775ed8d5a
Slava Zakharov <slava.zakharov@mailbox.org>
2024-05-07 12:56:13 +0400

    Add a landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	redirects: {
+		"/podcasts": {
+			status: 302,
+			destination: "https://deepcuts.ru/podcast",
+		},
+		"/running": {
+			status: 302,
+			destination: "https://www.strava.com/athletes/petyapetr",
+		},
+	},
+});
